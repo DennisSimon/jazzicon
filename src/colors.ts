9685422fe77fc15956232e1452df2bee642dadfd
Dennis Simon <dennism.simon94@gmail.com>
2022-11-28 21:44:09 +0900

    fix color calculations

--- src/colors.ts
+++ src/colors.ts
@@ -35,9 +35,9 @@ function hexToHSL(hex: string): HSL {
   let g = "0x" + hex[3] + hex[4];
   let b = "0x" + hex[5] + hex[6];
   // Then to HSL
-  const HSLr = parseFloat(r) / 255;
-  const HSLg = parseFloat(g) / 255;
-  const HSLb = parseFloat(b) / 255;
+  const HSLr = +r / 255;
+  const HSLg = +g / 255;
+  const HSLb = +b / 255;
   const cmin = Math.min(HSLr, HSLg, HSLb),
     cmax = Math.max(HSLr, HSLg, HSLb),
     delta = cmax - cmin;
@@ -63,51 +63,15 @@ function hexToHSL(hex: string): HSL {
 }
 
 function HSLToHex(hsl: HSL) {
-  var { h, s, l } = hsl;
-  s /= 100;
+  let { h, s, l } = hsl;
   l /= 100;
-
-  let c = (1 - Math.abs(2 * l - 1)) * s,
-    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
-    m = l - c / 2,
-    r = 0,
-    g = 0,
-    b = 0;
-
-  if (0 <= h && h < 60) {
-    r = c;
-    g = x;
-    b = 0;
-  } else if (60 <= h && h < 120) {
-    r = x;
-    g = c;
-    b = 0;
-  } else if (120 <= h && h < 180) {
-    r = 0;
-    g = c;
-    b = x;
-  } else if (180 <= h && h < 240) {
-    r = 0;
-    g = x;
-    b = c;
-  } else if (240 <= h && h < 300) {
-    r = x;
-    g = 0;
-    b = c;
-  } else if (300 <= h && h < 360) {
-    r = c;
-    g = 0;
-    b = x;
-  }
-  // Having obtained RGB, convert channels to hex
-  r = Math.round((r + m) * 255);
-  g = Math.round((g + m) * 255);
-  b = Math.round((b + m) * 255);
-  //   r = Math.round((r + m) * 255).toString(16);
-  //   g = Math.round((g + m) * 255).toString(16);
-  //   b = Math.round((b + m) * 255).toString(16);
-
-  const toPaddedHex = (num: number) => num.toString(16).padStart(2, "0");
-
-  return `#${[r, g, b].map(toPaddedHex).join("")}`;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
 }
